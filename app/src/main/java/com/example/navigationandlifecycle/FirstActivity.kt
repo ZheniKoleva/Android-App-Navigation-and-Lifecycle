023c167920c316c1208dbac1b96411138889c17c
ZheniKoleva <zhenie_koleva@abv.bg>
2022-10-26 21:48:51 +0300

    files added

---
+++ app/src/main/java/com/example/navigationandlifecycle/FirstActivity.kt
@@ -0,0 +1,24 @@
+package com.example.navigationandlifecycle
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.navigationandlifecycle.databinding.ActivityFirstBinding
+
+class FirstActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFirstBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityFirstBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+       binding.firstActivityButton.setOnClickListener {
+           val intent = Intent(this, SecondActivity::class.java)
+           startActivity(intent)
+
+       }
+    }
+}
